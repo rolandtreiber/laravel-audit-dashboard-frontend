5a26991ed223f70928ef93425f326be879d414da
Roland Treiber <info@thecaringdeveloper.com>
2023-03-16 16:57:17 +0000

    =initial commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import {parseDiff, Diff, Hunk} from 'react-diff-view';
+import {diffLines, formatLines} from 'unidiff';
+import 'react-diff-view/style/index.css';
 
 function App() {
+    const EMPTY_HUNKS = [];
+
+    const oldText = '[\n' +
+        '    {\n' +
+        '        "age": "22",\n' +
+        '        "name": "Niroj"\n' +
+        '    },\n' +
+        '    {\n' +
+        '        "age": "20",\n' +
+        '        "name": "Dey"\n' +
+        '    }\n' +
+        ']\n';
+    const newText = '[\n' +
+        '    {\n' +
+        '        "age": "22",\n' +
+        '        "name": "Niroj"\n' +
+        '    },\n' +
+        '    {\n' +
+        '        "age": "20",\n' +
+        '        "name": "Dey1"\n' +
+        '    }\n' +
+        ']\n';
+
+    const diffText = formatLines(diffLines(oldText, newText), {context: 3});
+    const [diff] = parseDiff(diffText, {nearbySequences: 'zip'});
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+          <Diff viewType="split" diffType='' hunks={diff.hunks || EMPTY_HUNKS}>
+              {hunks =>
+                  hunks.map(hunk => (
+                      <Hunk key={hunk.content} hunk={hunk} />
+                  ))
+              }
+          </Diff>
       </header>
     </div>
   );
